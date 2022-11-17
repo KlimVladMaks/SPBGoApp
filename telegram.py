b78230fd8c2d78c703a572558bcd4b1460f6c031
bog <dany.as@yandex.ru>
2022-11-17 18:59:55 +0300

    added github workflow

---
+++ telegram.py
@@ -0,0 +1,10 @@
+import os
+import telebot
+
+API_TOKEN = os.getenv('API')
+chat_id = os.getenv('CHAT')
+bot = telebot.TeleBot(API_TOKEN, parse_mode=None)
+
+doc = open('app.apk', 'rb')
+bot.send_document(chat_id, doc)
+bot.send_document(chat_id, "FILEID")
