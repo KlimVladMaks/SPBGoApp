5c6965991bc6cd122f091de10ef839fab8b8803a
bog <dany.as@yandex.ru>
2022-11-17 22:49:41 +0300

    try another path to apk

--- telegram.py
+++ telegram.py
@@ -6,6 +6,6 @@ API_TOKEN = os.getenv('API')
 chat_id = os.getenv('CHAT')
 bot = telebot.TeleBot(API_TOKEN, parse_mode=None)
 
-doc = open('tmp/app-debug.apk', 'rb')
+doc = open('app-debug.apk', 'rb')
 bot.send_document(chat_id, doc)
 bot.send_document(chat_id, "FILEID")
