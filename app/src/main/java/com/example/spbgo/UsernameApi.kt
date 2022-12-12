0b9186dfe3e42c79b056c594f0b17778e1ac7174
Vlad <113041121+KlimVladMaks@users.noreply.github.com>
2022-12-12 19:41:11 +0300

    Класс для получения имени пользователя

---
+++ app/src/main/java/com/example/spbgo/UsernameApi.kt
@@ -0,0 +1,58 @@
+package com.example.spbgo
+
+import org.json.JSONObject
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+// Класс для получения имени пользователя с сервера
+class UsernameApi() {
+
+    // URL для обращения к серверу
+    private val url = "http://77.234.215.138:60866/spbgo/api/profile"
+
+    // Функция для отапрвки запроса к серверу
+    fun getResponse(access_token: String): String {
+
+        // Строка для записи ответа от сервера
+        val usernameResponseText: String
+
+        // Строка для записи имени пользователя
+        val username: String
+
+        // Устанавливаем соединения с сервером
+        val httpURLConnection = URL(url).openConnection() as HttpURLConnection
+
+        try {
+            // Указываем параметры GET-запроса
+            httpURLConnection.setRequestProperty("Access-Token", access_token)
+            httpURLConnection.requestMethod = "GET"
+            httpURLConnection.doInput = true
+            httpURLConnection.connectTimeout = 300000
+
+            // Считываем входящие данные в формате строки
+            val streamReader = InputStreamReader(httpURLConnection.inputStream)
+            streamReader.use {
+                usernameResponseText = it.readText()
+            }
+
+            // Преобразуем входящие данные в JSON-объект
+            val usernameJsonObject = JSONObject(usernameResponseText)
+            // Вытаскиваем имя пользователя из JSON-объекта
+            username = usernameJsonObject.getString("login")
+
+        } catch (e: Exception) {
+
+            // В случае ошибки возвращаем пустую строку
+            return ""
+
+        } finally {
+
+            // Разрываем соединение
+            httpURLConnection.disconnect()
+        }
+
+        // Возвращаем имя пользователя
+        return username
+    }
+}
